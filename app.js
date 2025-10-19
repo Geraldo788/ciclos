3e9d083449ad558b528af3f3a81a7949352a5bdb
Geraldo <geraldo7888@gmail.com>
2025-10-18 19:34:55 -0500

    inicia clase de ciclos

---
+++ app.js
@@ -0,0 +1,21 @@
+// let entrada = parseInt(prompt("Ingrese numero de tabla a multiplicar:"))
+
+// if (!isNaN (entrada)) {
+//      for (let i = 1; i <=10; i++) {
+//         let resultado = entrada *i;
+//     if (resultado % 2 === 0) {
+//            console.log(`${entrada} x ${i} = ${resultado}`);
+ 
+//     }else { 
+// console.log("por favor ingrese un valor valido");
+    
+//  } 
+//  }
+
+// }
+
+
+
+let numero = parseInt(prompt("ingrese el numero"))
+
+let primo = 
